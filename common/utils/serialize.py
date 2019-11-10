fe25ed6c705487727a8bf2a8e3287a68bd683c6b
xiayunil <492988836@qq.com>
2019-11-10 18:03:13 +0800

    serialize rewrite

--- common/utils/serialize.py
+++ common/utils/serialize.py
@@ -1,5 +1,6 @@
 import json
-
+from models.business.chain_info import *
+from models.user.user_info import *
 
 class Serializer(object):
     @classmethod
@@ -8,7 +9,8 @@ class Serializer(object):
 
     @classmethod
     def toDict(cls, o: object) -> dict:
-        if not isinstance(o, (dict, list, tuple, str, int, float, bool, None)):
+        d=o
+        if not isinstance(o, (dict, list, tuple, str, int, float, bool)):
             d = {"serialized_tag_type": type(o).__name__,
                  "serialized_tag_data": o.__dict__}
             for k, v in d["serialized_tag_data"].items():
@@ -20,14 +22,20 @@ class Serializer(object):
     # 反序列化,需要检查当前类型
     # 目前简单的重载了类数据,之后可以考虑根据type动态生成对象
     @classmethod
-    def deSerialize(cls, data: json, o: object) -> object:
+    def deSerialize(cls, data: json) -> object:
         #TODO 反射
         jsonData = json.loads(data)
-        if type(o).__name__ != jsonData["type"]:
-            raise TypeError("object type: %s not match json type:%s" %
-                            (type(o), jsonData["type"]))
+        return cls.gen_obj(jsonData)
+
+    @classmethod
+    def gen_obj(cls,d:dict)->object:
+        if "serialized_tag_type" in d:
+            if d["serialized_tag_type"] not in globals():
+                raise TypeError("object type: %s not implemented" % (d["serialized_tag_type"]))
+            
+            obj=globals()[d["serialized_tag_type"]]()
+            for k, v in d["serialized_tag_data"].items():
+                setattr(obj,k,cls.gen_obj(v))
+            return obj
         else:
-            data = jsonData["data"]
-            for k, v in data.items():
-                setattr(o, k, v)
-            return o
+            return d
