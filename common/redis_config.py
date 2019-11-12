d3b502db8484b6d0f9e0a31ca8244cfba750c6a2
TemplarJQ <jqliu_bupt@163.com>
2019-11-12 23:04:11 +0800

    change redis password

--- common/redis_config.py
+++ common/redis_config.py
@@ -4,7 +4,7 @@ from enum import Enum
 
 
 class REDIS_DB_URL(Enum):
-        HOST = "127.0.0.1"  # 主机位置
+        HOST = "47.103.27.246"  # 主机位置
         PORT = 6379  # 连接端口
-        PASSWORD = None  # 连接密码
+        PASSWORD = "bupt"  # 连接密码
         DB = 0  # 所连数据库位置
