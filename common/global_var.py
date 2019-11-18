db7ddfe4c6b75675741de8b3c0afbdd4790e6965
xiayunil <492988836@qq.com>
2019-11-18 20:39:03 +0800

    .

--- common/global_var.py
+++ common/global_var.py
@@ -2,31 +2,110 @@ from threading import Lock
 from models.user.user_info import UserService
 # 定义相关的全局名单对迁移信息进行访问
 # 其内部为用户ID，便于查询是否在迁移序列中
-class global_var(object):
-    
-    __migration_lock=Lock()
-    __user_service_lock=Lock()
-    __GLOBAL_USER_SERVICE_LIST :map={}
-    __GLOBAL_MIGRATION_LIST :map={}
 
-    @classmethod 
+
+# 对应结构:__GLOBAL_USER_SERVICE_MAP,__GLOBAL_MIGRATION_MAP都为
+# map,map中key为user_id
+# value为保存当前user_id下所有service的map
+# value的map以service_id做key,value为UserService对象
+
+class service_map(object):
+
+    __migration_lock = Lock()
+    __user_service_lock = Lock()
+    __GLOBAL_USER_SERVICE_MAP: map = {}
+    __GLOBAL_MIGRATION_MAP: map = {}
+
+    @classmethod
     # 如果 us不在map中,返回false,us为空
-    def get_migration_service(cls,user_token:int)->(bool,UserService):
+    def get_migration_service(cls, user_token: int,service_id:int) -> (bool, UserService):
         cls.__migration_lock.acquire()
-        if user_token not in cls.__GLOBAL_MIGRATION_LIST:
+        if user_token not in cls.__GLOBAL_MIGRATION_MAP:
             cls.__migration_lock.release()
-            return False,""
+            return False, None
         else:
+            us_map = cls.__GLOBAL_MIGRATION_MAP[user_token]
+            if service_id not in us_map:
+                return False, None
+            us=cls.__GLOBAL_MIGRATION_MAP[service_id]
             cls.__migration_lock.release()
-            return True,cls.__GLOBAL_MIGRATION_LIST[user_token]
+            return True, us
 
-    @classmethod 
+    @classmethod
     # 如果 us不在map中,返回false,us为空
-    def get_user_service(cls,user_token:int)->(bool,UserService):
+    def get_user_service(cls, user_token: int,service_id:int) -> (bool, UserService):
         cls.__user_service_lock.acquire()
-        if user_token not in cls.__GLOBAL_USER_SERVICE_LIST:
+        if user_token not in cls.__GLOBAL_USER_SERVICE_MAP:
             cls.__user_service_lock.release()
-            return False,""
+            return False, None
         else:
+            us_map = cls.__GLOBAL_USER_SERVICE_MAP[user_token]
+            if service_id not in us_map:
+                return False, None
+            us= cls.__GLOBAL_USER_SERVICE_MAP[service_id]
             cls.__user_service_lock.release()
-            return True,cls.__GLOBAL_USER_SERVICE_LIST[user_token]
+            return True, us
+
+    @classmethod
+    # 如果 us不在map中,返回false,us为空
+    def get_all_migration_service(cls, user_token: int) -> (bool, map(int,UserService)):
+        cls.__migration_lock.acquire()
+        if user_token not in cls.__GLOBAL_MIGRATION_MAP:
+            cls.__migration_lock.release()
+            return False, None
+        else:
+            us_map = cls.__GLOBAL_MIGRATION_MAP[user_token]
+            cls.__migration_lock.release()
+            return True, us_map
+
+    @classmethod
+    # 如果 us不在map中,返回false,us为空
+    def get_all_user_service(cls, user_token: int) -> (bool, map(int,UserService)):
+        cls.__user_service_lock.acquire()
+        if user_token not in cls.__GLOBAL_USER_SERVICE_MAP:
+            cls.__user_service_lock.release()
+            return False, None
+        else:
+            us_map = cls.__GLOBAL_USER_SERVICE_MAP[user_token]
+            cls.__user_service_lock.release()
+            return True, us_map
+
+    @classmethod
+    def set_migration_service(cls, us: UserService) -> bool:
+        if type(us) is not UserService:
+            raise TypeError
+        cls.__user_service_lock.acquire()
+        if us.service_token.user_id in cls.__GLOBAL_MIGRATION_MAP:
+            # 当前service_id已经存入,不能再存,返回false
+            if us.service_token.service_id in  cls.__GLOBAL_MIGRATION_MAP[us.service_token.user_id]:
+                return False
+            # 新存入一个,返回true    
+            cls.__GLOBAL_MIGRATION_MAP[us.service_token.user_id][us.service_token.service_id]=us
+            cls.__user_service_lock.release()
+            return True
+        else:
+            # 新建map,存入对象
+            cls.__GLOBAL_MIGRATION_MAP[us.service_token.user_id]={}
+            cls.__GLOBAL_MIGRATION_MAP[us.service_token.user_id][us.service_token.service_id]=us
+            cls.__user_service_lock.release()
+            return True
+
+    @classmethod
+    def set_user_service(cls, us: UserService) -> bool:
+        if type(us) is not UserService:
+            raise TypeError
+        cls.__user_service_lock.acquire()
+        if us.service_token.user_id in cls.__GLOBAL_USER_SERVICE_MAP:
+            # 当前service_id已经存入,不能再存,返回false
+            if us.service_token.service_id in  cls.__GLOBAL_USER_SERVICE_MAP[us.service_token.user_id]:
+                return False
+            # 新存入一个,返回true    
+            cls.__GLOBAL_USER_SERVICE_MAP[us.service_token.user_id][us.service_token.service_id]=us
+            cls.__user_service_lock.release()
+            return True
+        else:
+            # 新建map,存入对象
+            cls.__GLOBAL_USER_SERVICE_MAP[us.service_token.user_id]={}
+            cls.__GLOBAL_USER_SERVICE_MAP[us.service_token.user_id][us.service_token.service_id]=us
+            cls.__user_service_lock.release()
+            return True
