2c7e3310e6ab83eb92c65ab7f1106adb062fe6c7
xiayunil <492988836@qq.com>
2019-11-24 17:37:27 +0800

    add func for maps

--- routes/main/handler.py
+++ routes/main/handler.py
@@ -1,7 +1,6 @@
 from flask import request
 import requests
 
-from common import global_var
 from models.business.chain_info import *
 from models.user.user_info import *
 from common.utils.logger import logger
@@ -9,48 +8,69 @@ from common.utils.logger import logger
 from common.global_var import service_map
 from common.utils.logger import logger
 
-# TODO 传引用
-def handel_service(user_token: int,service_token: int):
-    # 从offset恢复对应的服务
-    is_In_map,us=service_map.get_user_service(user_token,service_token)
-    if not is_In_map:
-        logger.warn("not in service map")
-        return
+from concurrent.futures import ThreadPoolExecutor
 
-    us.lock_userService()
-    offset,chain_length=us.service_bus.chain_offset,us.service_chain.num
-    us.unlock_userService()
 
-    for i in range(offset,
-                   chain_length):
+class compute_handler(object):
 
-        #获取miniservice对应地址
-        # 必须先处理完毕  
-        us.lock_userService()         
-        minServiceAddr,us_data = us.service_chain.mini_service[i],us.service_bus.data
-        us.unlock_userService()
-        
-        res=requests.post(minServiceAddr, data=us_data)
+    pool = ThreadPoolExecutor(1024)
 
-        # 从json文件中中获取data传输过来的data
-        if res.status_code == 200:
-            data = res.raw.read()
-        if res.status_code != 200:
-            logger.warn("receive non-200 return without doing anything ")
-            return 
+    @classmethod
+    def compute_us_func(cls, us: UserService):
+        user_token, service_token = us.service_token.user_id, us.service_token.service_id
+        cls.pool.submit(cls.handel_service, (user_token, service_token))
+   
+    @classmethod
+    def mig_func(cls,us: UserService):
+        pass
 
-        us.lock_userService()
-        is_migration=us.service_bus.is_migration
-        us.unlock_userService()
-        if is_migration:
-            logger.warn("migration begins")
-            #Todo : 处理中断逻辑
+    @classmethod
+    def register_func(cls):
+        service_map.register_us_func( cls.compute_us_func)
+        service_map.register_migration_func(cls.mig_func)
+
+    @classmethod
+    def handel_service(user_token: int, service_token: int):
+        # 从offset恢复对应的服务
+        is_In_map, us = service_map.get_user_service(user_token, service_token)
+        if not is_In_map:
+            logger.warn("not in service map")
             return
 
         us.lock_userService()
-        # 更新data
-        us.service_bus.data= data
-        #增加offset
-        us.service_bus.chain_offset+=1
+        offset, chain_length = us.service_bus.chain_offset, us.service_chain.num
         us.unlock_userService()
 
+        for i in range(offset,
+                       chain_length):
+
+            # 获取miniservice对应地址
+            # 必须先处理完毕
+            us.lock_userService()
+            minServiceAddr, us_data = us.service_chain.mini_service[i], us.service_bus.data
+            us.unlock_userService()
+
+            res = requests.post(minServiceAddr, data=us_data)
+
+            # 从json文件中中获取data传输过来的data
+            if res.status_code == 200:
+                data = res.raw.read()
+            if res.status_code != 200:
+                logger.warn("receive non-200 return without doing anything ")
+                # TODO re-try
+                return
+
+            us.lock_userService()
+            is_migration = us.service_bus.is_migration
+            if is_migration:
+                # 需要处理migration逻辑,立即释放锁
+                us.unlock_userService()
+                logger.warn("migration begins")
+                #Todo : 处理中断逻辑
+                return
+
+            # 更新data
+            us.service_bus.data = data
+            # 增加offset
+            us.service_bus.chain_offset += 1
+            us.unlock_userService()
